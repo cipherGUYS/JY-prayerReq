0fffde6dcb7ba46c75390a2fcceec7c2f89e1608
Tomin-Joy <tominjk007@gmail.com>
2022-05-07 00:06:24 +0530

    Telegram support added

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbz3PNuRcDbEzlKKjfV3kY
 const form = document.forms['prayer-request']
 
 form.addEventListener('submit', e => {
+    var name1 = form['name'].value;
+    var class1 = form['class'].value;
+    var re1 = form['request'].value;
+    fetch(`https://api.telegram.org/bot5270203551:AAHmDszAG_3LpScU-EO6xamxV1kBuBN-tqs/sendMessage?chat_id=502916042&text=name%20:%20${name1}%0Aclass%20:%20${class1}%0Arequest%20:%20${re1}`);
+    fetch(`https://api.telegram.org/bot5270203551:AAHmDszAG_3LpScU-EO6xamxV1kBuBN-tqs/sendMessage?chat_id=-737082532&text=name%20:%20${name1}%0Aclass%20:%20${class1}%0Arequest%20:%20${re1}`);
+    
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(() => {
