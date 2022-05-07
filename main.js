baf599717ce68b1e6ebf723947981b8fb3ba90ac
Tomin-Joy <tominjk007@gmail.com>
2022-05-07 17:12:49 +0530

    reverted back

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz3PNuRcDbEzlKKjfV3kYGCBqeBddyCelwOXYqPIW_oOWwo2Fzr-Kj4YmxQD0n6XLEUNQ/exec'
 const form = document.forms['prayer-request']
-env:
-  token=${{ secrets.TELE_BOT }};
+
 form.addEventListener('submit', e => {
     var name1 = form['name'].value;
-    var year1 = form['year'].value;
+    var class1 = form['class'].value;
     var re1 = form['request'].value;
-    fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=-737082532&Text=name%20:%20${name1}%0AYear%20:%20${year1}%0ARequest%20:%20${re1}`);
+    fetch(`https://api.telegram.org/bot5270203551:AAHmDszAG_3LpScU-EO6xamxV1kBuBN-tqs/sendMessage?chat_id=-737082532&text=name%20:%20${name1}%0Aclass%20:%20${class1}%0Arequest%20:%20${re1}`);
     
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
